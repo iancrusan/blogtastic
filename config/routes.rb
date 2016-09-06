076de0423316e3a7fbf33084b224e7a7f66fd499
ian crusan <iancrusan@hotmail.com>
2016-09-05 21:59:24 -0400

    added comments edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :blog_posts do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :edit, :update, :destroy]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
