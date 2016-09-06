659f36aed47b6e925cdfca93989bfdf9fc3eb51b
ian crusan <iancrusan@hotmail.com>
2016-09-06 16:28:06 -0400

    devise up and running

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,6 +13,6 @@ end
 private
 
 def comment_params
-  params.require(:comment).permit(:author, :comment_entry, :blog_post_id)
+  params.require(:comment).permit(:author, :comment_entry, :blog_post_id, :user_id)
 end
 end
